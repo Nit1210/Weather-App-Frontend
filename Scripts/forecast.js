eab84a46fc9b07797b0a4dbb1d0570ca81f54882
Nitesh Kalyan Koilada <98055206+Nit1210@users.noreply.github.com>
2023-11-25 00:48:19 +0000

    Pending changes exported from your codespace

---
+++ Scripts/forecast.js
@@ -0,0 +1,12 @@
+const key="f71743e7572745e0a1b223132232311";
+
+// const city="dallas";
+
+const getCityWeather= async (city)=>{
+    const url=`https://api.weatherapi.com/v1/current.json?key=f71743e7572745e0a1b223132232311&q=${city}&aqi=yes`;
+    const response= await fetch(url);
+    const responseData=await response.json();
+   
+   return {citydata: responseData.location , weatherData: responseData.current} ;
+    // return responseData;
+};
