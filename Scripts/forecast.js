5ec9c73c05228d3c86f29a97f48068648f22bf5d
Nit1210 <98055206+Nit1210@users.noreply.github.com>
2023-11-24 21:13:59 -0500

    Updated: "Error statement for api function"

--- Scripts/forecast.js
+++ Scripts/forecast.js
@@ -5,6 +5,9 @@ const key="f71743e7572745e0a1b223132232311";
 const getCityWeather= async (city)=>{
     const url=`https://api.weatherapi.com/v1/current.json?key=f71743e7572745e0a1b223132232311&q=${city}&aqi=yes`;
     const response= await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Error fetching data for ${city}`);
+    }
     const responseData=await response.json();
    
    return {citydata: responseData.location , weatherData: responseData.current} ;
