b3c25347fe65dfb21fedf32a26b44a3ee78c426d
Nitesh Kalyan Koilada <98055206+Nit1210@users.noreply.github.com>
2024-01-20 02:05:48 +0000

    updates API KEY

--- Scripts/forecast.js
+++ Scripts/forecast.js
@@ -1,10 +1,10 @@
 class Forecast{
     constructor(){
-     
+         this.key="c97aff7aede3467a97414532242001";
     }
     
     async  getCityWeather(city) {
-        this.URL=`https://api.weatherapi.com/v1/current.json?key=c97aff7aede3467a97414532242001&q=${city}&aqi=yes`;
+        this.URL=`https://api.weatherapi.com/v1/current.json?key=${this.key}&q=${city}&aqi=yes`;
         const response= await fetch(this.URL);
     if (!response.ok) {
         throw new Error(alert(`Enter valid location. Error fetching data for ${city}`));
